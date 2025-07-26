1f06c50c52263cb2795e8bf0d2095af74d65f145
jy S <04jysong@naver.com>
2025-07-27 00:38:23 +0900

    modified:   bj10811.py

--- bj10811.py
+++ bj10811.py
@@ -1,19 +1,8 @@
 a=[]
-temp = 0
-l=[]
 n,m = map(int,input().split())
 for i in range(1,n+1):
     a.append(i)
-for i in range(m):
+for u in range(m):
     i,j = map(int,input().split())
-    for t in range(i,j+1):
-        # temp = a[i-1]
-        # a[i-1] = a[j-1]
-        # a[j-1] = temp
-        # for k in range(i,j+1):
-        #     l.append(a[k-1])
-        # l = l[::-1]
-        # for u in range(i,j+1):
-        #     a.insert((u-1),l[k-1])
-        a = a
+    a[i-1:j] = a[i-1:j][::-1]
 print(*a)
