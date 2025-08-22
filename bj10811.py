9b59f7b7882d8e80ce541da8933b479b12e12c57
jy S <04jysong@naver.com>
2025-08-22 19:28:49 +0900

    modified:   bj10811.py

--- bj10811.py
+++ bj10811.py
@@ -4,5 +4,6 @@ for i in range(1,n+1):
     a.append(i)
 for u in range(m):
     i,j = map(int,input().split())
-    a[i-1:j] = a[i-1:j][::-1]
-print(*a)
+    a[i-1:j] = a[i-1:j][::-1] #슬라이싱에 대해서 자세히 파악하기. 시작점,끝점은미포함인데 시작점이 끝점보다 클 수 없다. 순서를 역순하고 싶을때는 2개의 대괄호를 써야함
+    #반드시 다시 풀어볼것 
+print(*a) 
