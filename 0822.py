cd074f13293c0a79fef544609264c99aa19dee16
jy S <04jysong@naver.com>
2025-08-22 19:28:28 +0900

    new file:   0822.py

---
+++ 0822.py
@@ -0,0 +1,23 @@
+# #2675번.
+
+# a =int(input())
+# for i in range(a):
+#     b= input().split()
+#     k = b[1]
+#     c=[]
+#     for ch in k:
+#         ny = ch*int(b[0])
+#         c.append(ny)
+#     print(''.join(c))#리스트에서 모든 공백을 제거하고 싶을때. 리스트들 항목들간의 공백도 제거하고 싶은경우
+
+#2869번
+
+time = map(int,input().split())
+count = 0
+
+
+
+
+
+    
+    
